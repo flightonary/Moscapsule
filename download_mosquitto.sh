05c3859c21872428c923cb40ddbc2673291a6ebc
flightonary <flightonary@users.noreply.github.com>
2014-12-15 13:22:51 +0900

    add OpenSSL as submodule and add download_mosquitto.sh

---
+++ download_mosquitto.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MOSQ_VERSION="1.3.5"
+MOSQ_URL="http://mosquitto.org/files/source/mosquitto-${MOSQ_VERSION}.tar.gz"
+
+TMP_FILE="/tmp/mosquitto-${MOSQ_VERSION}.tar.gz"
+
+# download & copy
+curl -o $TMP_FILE $MOSQ_URL
+tar zxf $TMP_FILE -C ${TMP_FILE%/*}
+rsync -av --delete ${TMP_FILE%.tar.gz}/ ./mosquitto
+
+# clean up
+rm -rf ${TMP_FILE%-*}*
